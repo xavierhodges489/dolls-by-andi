9c86c2065d80d6bb76460360ef6cabf342a6cc1c
Xavier Hodges <xavierhodges489@gmail.com>
2020-03-07 21:25:22 -0500

    made position absolute to not affect other elements

--- src/components/catalogCard.js
+++ src/components/catalogCard.js
@@ -5,28 +5,30 @@ import buttonStyles from "../styles/buttons.module.scss"
 
 const CatalogCard = props => {
   return (
-    <div className={catalogCardStyles.card}>
-      <div className={catalogCardStyles.imgContainer}>
-        <img src={props.imgUrl} alt={props.name}></img>
-        <div></div>
-      </div>
-      <div className={catalogCardStyles.contentContainer}>
-        <div>
-          <h3>{props.name}</h3>
-          <span className={catalogCardStyles.price}>${props.price}</span>
+    <div className={catalogCardStyles.cardContainer}>
+      <div className={catalogCardStyles.card}>
+        <div className={catalogCardStyles.imgContainer}>
+          <img src={props.imgUrl} alt={props.name}></img>
+          <div></div>
         </div>
-        {props.tag && (
-          <span className={catalogCardStyles.tag}>{props.tag}</span>
-        )}
+        <div className={catalogCardStyles.contentContainer}>
+          <div>
+            <h3>{props.name}</h3>
+            <span className={catalogCardStyles.price}>${props.price}</span>
+          </div>
+          {props.tag && (
+            <span className={catalogCardStyles.tag}>{props.tag}</span>
+          )}
 
-        <p
-          className={
-            props.tag ? catalogCardStyles.oneLine : catalogCardStyles.twoLines
-          }
-        >
-          {props.description}
-        </p>
-        <button className={buttonStyles.button}>Purchase</button>
+          <p
+            className={
+              props.tag ? catalogCardStyles.oneLine : catalogCardStyles.twoLines
+            }
+          >
+            {props.description}
+          </p>
+          <button className={buttonStyles.button}>Purchase</button>
+        </div>
       </div>
     </div>
   )
